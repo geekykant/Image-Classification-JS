af94d2d9647ff8e1427be2af08c7f68c33d2dd25
Sreekant <geekykant@gmail.com>
2019-01-18 16:03:11 +0530

    intial commit

--- sketch.js
+++ sketch.js
@@ -1,31 +1,37 @@
 let mobilenet;
+let video;
 
-let muffin;
+let label = '';
 
 function modelReady() {
   console.log("Model is ready!!");
-}
-
-function imageReady() {
-  image(muffin, 0, 0, width, height);
-  mobilenet.predict(muffin, gotResults);
+  mobilenet.predict(video, gotResults);
 }
 
 function gotResults(err, results) {
   if (err) {
     console.log("Error: " + err);
   } else {
-    console.log(results);
-    let label = results[0].className;
-    createElement('h1', label);
+    // console.log(results);
+    label = results[0].className;
+    mobilenet.predict(video, gotResults);
   }
 }
 
 function setup() {
-  createCanvas(321, 240);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.hide();
   background(0);
+  // muffin = createImg("images/muffin.jpg", imageReady);
+  // muffin.hide();
+  mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+}
 
-  muffin = createImg("images/muffin.jpg", imageReady);
-  muffin.hide();
-  mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+
+function draw() {
+  image(video, 0, 0);
+  fill(255);
+  textSize(34);
+  text(label, 10, height - 100);
 }
