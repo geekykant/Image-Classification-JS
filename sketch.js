4fd4a797df867bb426758866615ea60b02f5cd92
Sreekant <geekykant@gmail.com>
2019-01-19 10:37:44 +0530

    model saving added

--- sketch.js
+++ sketch.js
@@ -1,15 +1,20 @@
 let video;
 let mobilenet;
 
-let predictor;
-let value = 0;
-let slider;
+let classifier;
+let label = 'loading model';
 let trainButton;
-
-let addButton;
+let happyButton, sadButton;
 
 function modelReady() {
   console.log("Model is ready!!");
+  classifier.load('model/model.json', customModelReady);
+}
+
+function customModelReady() {
+  console.log("Custom model ready!");
+  label = 'model ready';
+  classifier.classify(gotResults);
 }
 
 function videoReady() {
@@ -20,19 +25,19 @@ function gotResults(err, result) {
   if (err) {
     console.log("Error: " + err);
   } else {
-    value = result;
-    predictor.predict(gotResults);
+    label = result;
+    classifier.classify(gotResults);
   }
 }
 
-function whileTraining(loss) {
-  if (loss == null) {
-    console.log("Completed training!");
-    predictor.predict(gotResults);
-  } else {
-    console.log("Loss: " + loss);
-  }
-}
+// function whileTraining(loss) {
+//   if (loss == null) {
+//     console.log("Completed training!");
+//     classifier.classify(gotResults);
+//   } else {
+//     console.log("Loss: " + loss);
+//   }
+// }
 
 function setup() {
   createCanvas(640, 480);
@@ -40,26 +45,28 @@ function setup() {
   video.hide();
   background(0);
   mobilenet = ml5.featureExtractor('MobileNet', modelReady);
-  predictor = mobilenet.regression(video, videoReady);
+  classifier = mobilenet.classification(video, videoReady);
 
-  slider = createSlider(0, 1, 0.5, 0.01);
-
-  trainButton = createButton('train');
-  trainButton.mousePressed(function() {
-    predictor.train(whileTraining);
-  });
-
-  addButton = createButton('Add image example');
-  addButton.mousePressed(() => predictor.addImage(slider.value()));
+  // happyButton = createButton('happy');
+  // sadButton = createButton('sad');
+  //
+  // happyButton.mousePressed(() => classifier.addImage('happy'));
+  // sadButton.mousePressed(() => classifier.addImage('sad'));
+  //
+  // trainButton = createButton('train');
+  // trainButton.mousePressed(function() {
+  //   classifier.train(whileTraining);
+  // });
+  //
+  // let saveButton = createButton('save');
+  // saveButton.mousePressed(function() {
+  //   classifier.save();
+  // });
 }
 
 function draw() {
   image(video, 0, 0);
-  rectMode(CENTER);
-  fill(255,0,0);
-  rect(value * width, height/2, 50, 250);
-
   fill(255);
   textSize(34);
-  text(value, 10, height - 100);
+  text(label, 10, height - 100);
 }
